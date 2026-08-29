37fd0d9c867842ad4adc54c1f9ac37bbae29b637
itsmanikandan73 <soulquiet8@gmail.com>
2026-08-29 09:02:11 +0530

    adding do_while_loop.c program

---
+++ old-programs/do_while_loop.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	int i = 5; 		//initilization
+	do {
+		puts("Buddy!");
+		i++;		//increment or decrement
+	}while(i<=4);	//condition
+
+	return EXIT_SUCCESS;
+}
