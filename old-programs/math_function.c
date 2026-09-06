967f8b22c84979e58cedfe058db690cc79420742
itsmanikandan73 <soulquiet8@gmail.com>
2026-09-06 10:32:59 +0530

    adding math function program

---
+++ old-programs/math_function.c
@@ -0,0 +1,23 @@
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+	
+	int x = 45;
+
+	//x = sqrt(x);
+	//x = pow(x,2);
+	//x = round(x);
+	//x = ceil(x);
+	//x = floor(x);
+	//x = abs(x);
+	//x = log(x);
+	//x = sin(x);
+	//x = cos(x);
+	x = tan(x);
+
+	printf("%d\n",x);
+
+	return 0;
+}
